1922f1434c7eb8be7560f1bb04777a1bceb04d10
Alessandro <alessandrovaru@gmail.com>
2025-01-11 11:34:58 -0400

    add initial implementation of unknown module

---
+++ src/03-unknown.ts
@@ -0,0 +1,15 @@
+let anyVar: any;
+anyVar = 1;
+anyVar = 'string';
+anyVar = true;
+anyVar = {};
+anyVar = [];
+
+let isNew: boolean = anyVar;
+
+let unknownVar: unknown;
+unknownVar = 1;
+unknownVar = 'string';
+unknownVar = true;
+unknownVar = {};
+unknownVar = [];
